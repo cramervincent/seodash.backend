4f0071c1a2d974b36f5904fd0364b9e48f456c73
Vincent Cramer <cramervincent@gmail.com>
2023-02-08 22:00:26 +0100

    auth

--- functions/functions.py
+++ functions/functions.py
@@ -1,15 +1,19 @@
 import requests
 
 async def scan_all_backlinks(url, target_url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        if target_url in response.text:
-            return True
-            # get da and pa (WIP)
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            if target_url in response.text:
+                return True
+                # get da and pa (WIP)
+            else:
+                return False
         else:
-            return False
-    else:
-        return await False
+            return await False
+    except:
+        return False
+
 
 
 
