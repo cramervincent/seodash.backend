a7eca445c33cb9f1bf158f0ced446dd5c6f31142
Vincent <vincent@3wmedia.nl>
2023-02-09 14:39:15 +0100

    postgresql in production

--- dependencies/database.py
+++ dependencies/database.py
@@ -1,8 +1,19 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+load_dotenv()
+PRODUCTION_DB = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PSW')}@{os.getenv('DB_HOST')}"
+DEVELOPMENT_DB = "sqlite:///./db/database.db"
+print (PRODUCTION_DB)
+# os.getenv('JWT_SECRET')
+if os.getenv('ENVIROMENT') == 'production':
+    sql_db_url = PRODUCTION_DB
+else:
+    sql_db_url = DEVELOPMENT_DB
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./db/database.db"
+SQLALCHEMY_DATABASE_URL = sql_db_url
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
@@ -10,4 +21,4 @@ engine = create_engine(
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+Base = declarative_base() 
