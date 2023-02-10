d4e5d168b1a5666f7b3e900c7273ab946c91c85e
Vincent Cramer <cramervincent@gmail.com>
2023-02-10 18:06:24 +0100

    hex decode

--- dependencies/auth.py
+++ dependencies/auth.py
@@ -53,8 +53,7 @@ def decodeJWT(token: str) -> dict:
 def check_user(data: UserLoginSchema, db: Session):
     users = db.query(models.Users).all()
     for user in users:
-        userPass = user.password[2:].decode(hex)
-        print(user.password.encode(), '\n', hashPassword(data.password))
+        userPass = bytes.fromhex(user.password[2:]).decode("utf-8")
         if user.email == data.email and userPass == hashPassword(data.password):
             return True
 
