c20cfcbabbde56e02762dca74283d9fc747653dc
Vincent Cramer <cramervincent@gmail.com>
2023-02-10 17:34:47 +0100

    clear users

--- dependencies/database.py
+++ dependencies/database.py
@@ -4,9 +4,9 @@ from sqlalchemy.orm import sessionmaker
 import os
 from dotenv import load_dotenv
 load_dotenv()
-PRODUCTION_DB = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PSW')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PSW')}@{os.getenv('DB_PORT')}/{os.getenv('DB_PSW')}@{os.getenv('DB_DEF_DATABASE')}"
+PRODUCTION_DB = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PSW')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/@{os.getenv('DB_DEF_DATABASE')}"
 DEVELOPMENT_DB = "sqlite:///./db/database.db"
-
+print(PRODUCTION_DB)
 # os.getenv('JWT_SECRET')
 if os.getenv('ENVIROMENT') == 'production':
     sql_db_url = PRODUCTION_DB
